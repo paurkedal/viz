35003dfddc4e4e9d4bf000e9f81da56add41447d
Petter Urkedal <urkedal@nbi.dk>
2011-01-19 22:43:45 +0100

    Add dependency tracking and change build rules for Fform code.
    bin/ffoc1pp.ml:
      * Add --depend option to produce dependencies for ocamlbuild.
    myocamlbuild.ml, _tags:
      * Update to use the dependency tracker.
      * Invoke ffoc1pp with the -pp flag to ocamlc and ocamlopt, instead of
        the previous double-chained compilation.

--- bin/ffoc1pp.ml
+++ bin/ffoc1pp.ml
@@ -2,8 +2,33 @@ open Printf
 open Ffoc1
 open Diag
 open Camlp4.PreCast
+open FfPervasives
+open Cst_types
+open Ast_types
 
 let usage = "ffoc1 [--print | -o OUTPUT] INPUT"
+
+module String_set = Set.Make (String)
+
+let print_depend input_path m =
+    let rec extract comps = function
+	| [] -> comps
+	| Avar (_, Idr comp) :: vs -> extract (comp :: comps) vs in
+    let add_dep stra th =
+	let comps =
+	    match stra with
+	    | `Type | `Value | `Signature ->
+		let Apath (vs, _) = th in extract [] vs
+	    | `Structure ->
+		let Apath (vs, v) = th in extract [] (v :: vs) in
+	match comps with
+	| [] -> ident
+	| x :: xs -> String_set.add x in
+    let deps = Ast_utils.fold_amod_paths add_dep m String_set.empty in
+    print_string input_path;
+    print_char ':';
+    String_set.iter (fun c -> print_char ' '; print_string c) deps;
+    print_char '\n'
 
 let _ =
     let opt_setter r x =
@@ -12,10 +37,13 @@ let _ =
     let in_path_opt = ref None in
     let out_path_opt = ref None in
     let do_print = ref false in
+    let do_depend = ref false in
     let use_ast = ref true in
     let optspecs = Arg.align [
 	"-o", Arg.String (opt_setter out_path_opt),
 	    "PATH The output file.";
+	"--depend", Arg.Unit (fun () -> do_depend := true),
+	    " Output dependecies.";
 	"--print", Arg.Unit (fun () -> do_print := true),
 	    " Print expression tree.";
 	"--old-codegen", Arg.Unit (fun () -> use_ast := false),
@@ -36,6 +64,9 @@ let _ =
 	    printf "%s\n" (Formatter.contents fo)
 	end else begin
 	    try
+		if !do_depend then
+		    let amod = Cst_to_ast.build_amod term in
+		    print_depend in_path amod else
 		let omod =
 		    if !use_ast then
 			let amod = Cst_to_ast.build_amod term in
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,4 +1,4 @@
-(* Copyright 2010  Petter Urkedal
+(* Copyright 2010--2011  Petter Urkedal
  *
  * This file is part of Fform/OC <http://www.eideticdew.org/p/fform/>.
  *
@@ -16,18 +16,93 @@
  * along with Fform/OC.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
+open Ocamlbuild_pack
 open Ocamlbuild_plugin
 
-let ffoc1_path = "bin/ffoc1pp.native"
+let ffoc1pp_path = "bin/ffoc1pp.native"
 
-let ffoc1_cmd src dst env builder =
-    Cmd (S[A ffoc1_path; A"-o"; Px(env dst); P(env src)])
+(* Ocamlbuild Plug-In for Fform/OC
+ * =============================== *)
 
-let ffoc1_rules () =
-    rule "Fform/OC, Stage 1: ff -> ml"
-	~deps:["fflib/stdlex.ff"; "%.ff"; ffoc1_path]
-	~prods:["%.ml"]
-	(ffoc1_cmd "%.ff" "%.ml")
+(** A modified version of [Ocaml_compiler.ocamlc_c] which adds an "-impl" option
+    in front ofthe input file, so that ocamlc accepts a non-standard
+    extension. *)
+let custom_ocamlc_c tags arg out =
+    let tags = tags ++ "ocaml" ++ "byte" in
+    Cmd (S [!Options.ocamlc; A"-c"; T(tags ++ "compile");
+	    Ocaml_utils.ocaml_ppflags tags;
+	    Ocaml_utils.ocaml_include_flags arg; A"-o"; Px out; A"-impl"; P arg])
+
+(** A modified version of [Ocaml_compiler.ocamlopt_c] which adds an "-impl"
+    option in front of the input file, so that ocamlopt accepts a non-standard
+    extension. *)
+let custom_ocamlopt_c tags arg out =
+    let tags = tags ++ "ocaml" ++ "native" in
+    Cmd (S [!Options.ocamlopt; A"-c"; Ocaml_arch.forpack_flags_of_pathname arg;
+	    T(tags ++ "compile");
+	    Ocaml_utils.ocaml_ppflags tags;
+	    Ocaml_utils.ocaml_include_flags arg;
+	    A"-o"; Px out; A"-impl"; P arg])
+
+(** A modified version of [Ocaml_compiler.byte_compile_ocaml_implem] which uses
+    [custom_ocamlc_c]. *)
+let custom_byte_compile_ocaml_implem ?tag ff cmo env build =
+    let ff = env ff and cmo = env cmo in
+    Ocaml_compiler.prepare_compile build ff;
+    custom_ocamlc_c (tags_of_pathname ff ++ "implem" +++ tag) ff cmo
+
+(** A modified version of [Ocaml_compiler.native_compile_ocaml_implem] which
+    uses [custom_ocamlopt_c]. *)
+let custom_native_compile_ocaml_implem ?tag ?(cmx_ext = "cmx") ml env build =
+    let ml = env ml in
+    let cmi = Pathname.update_extensions "cmi" ml in
+    let cmx = Pathname.update_extensions cmx_ext ml in
+    Ocaml_compiler.prepare_link cmx cmi [cmx_ext; "cmi"] build;
+    custom_ocamlopt_c (Tags.union (tags_of_pathname ml) (tags_of_pathname cmx)
+		       ++ "implem" +++ tag) ml cmx
+
+(** Fform Stage 1 dependency analyzer. *)
+let ffoc1dep arg out env build =
+    let arg = env arg and out = env out in
+    let tags = tags_of_pathname arg ++ "ffoc1pp" ++ "ocamldep" in
+    Cmd(S[A ffoc1pp_path; T tags; A"--depend"; P arg; Sh ">"; Px out])
+
+(** Fform Stage 1 preprocessor subcommand. *)
+let ffoc1pp tag ff ff_ml env build =
+    let ff = env ff and ff_ml = env ff_ml in
+    let tags = tags_of_pathname ff ++ "ocaml" ++ "pp" ++ tag in
+    let _ = Rule.build_deps_of_tags build tags in
+    Cmd(S[A ffoc1pp_path; P ff; A"-o"; Px ff_ml])
+
+;;
+flag ["ocaml"; "pp"; "ffoc1pp"] (A ffoc1pp_path);;
+
+rule "Fform/OC Stage 1, Dependency Analysis"
+    ~tags:["ocaml"; "pp"; "ffoc1pp"]
+    ~prod:"%.ff.depends"
+    ~deps:["%.ff"; ffoc1pp_path; "fflib/stdlex.ff"]
+    (ffoc1dep "%.ff" "%.ff.depends");;
+
+rule "Fform/OC Stage 1, Byte Compilation: ff -> cmo & cmi"
+    ~tags:["ocaml"; "pp"; "ffoc1pp"]
+    ~prods:["%.cmo"; "%.cmi"]
+    ~deps:["%.ff"; "%.ff.depends"; "fflib/stdlex.ff"]
+    (custom_byte_compile_ocaml_implem "%.ff" "%.cmo");;
+
+rule "Fform/OC Stage 1, Native Compilation: ff -> cmx & o"
+    ~tags:["ocaml"; "pp"; "ffoc1pp"]
+    ~prods:["%.cmx"; "%.o"; "%.cmi"]
+    ~deps:["%.ff"; "%.ff.depends"; "fflib/stdlex.ff"]
+    (custom_native_compile_ocaml_implem "%.ff");;
+
+rule "Fform/OC Stage 1, Preprocessing Only: ff -> ff.ml"
+    ~dep:"%.ff"
+    ~prod:"%.ff.ml"
+    (ffoc1pp "ff.ml" "%.ff" "%.ff.ml");;
+
+
+(* Other Rules
+ * =========== *)
 
 let run_and_read = Ocamlbuild_pack.My_unix.run_and_read
 
@@ -38,7 +113,8 @@ let ocamlfind_query pkg =
 let ocaml_pkg lib =
     let tag = "use_" ^ lib in
     flag ["ocaml"; "compile"; tag] & S[A"-package"; A lib];
-    flag ["ocaml"; "link";    tag] & S[A"-package"; A lib]
+    flag ["ocaml"; "link";    tag] & S[A"-package"; A lib];
+    flag ["ocaml"; "doc";     tag] & S[A"-package"; A lib]
 
 let ocaml_pp lib pa =
     flag ["ocaml"; "pp"; pa]
@@ -50,7 +126,7 @@ let () = dispatch begin function
 	Options.use_menhir := true;
 	()
     | After_rules ->
-	ffoc1_rules ();
+	(* ffoc1_rules (); *)
 	flag ["ocaml"; "link"] & A"-linkpkg";
 	flag ["ocaml"; "compile"; "use_threads"] & A"-thread";
 	flag ["ocaml"; "link";    "use_threads"] & A"-thread";
