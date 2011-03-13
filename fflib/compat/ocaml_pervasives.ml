b17d498d67118fbf724dad2ed1d8cb66a2203b79
Petter Urkedal <paurkedal@gmail.com>
2011-03-13 17:23:08 +0100

    Add composition and application operators.
    This belongs to the previous commit.

---
+++ fflib/compat/ocaml_pervasives.ml
@@ -0,0 +1,50 @@
+(* Copyright 2011  Petter Urkedal
+ *
+ * This file is part of the Fform Standard Library.
+ *
+ * The Fform Standard Library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Fform is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fform.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+module Bool_ops = struct
+    let op1_U00ac = Pervasives.not
+    let op2_U2227 = Pervasives.( && )
+    let op2_U2228 = Pervasives.( || )
+
+    let op2_U003d = Pervasives.(=)
+    let op2_U2260 = Pervasives.(<>)
+    let op2_U003c = Pervasives.(<)
+    let op2_U003e = Pervasives.(>)
+    let op2_U2264 = Pervasives.(<=)
+    let op2_U2265 = Pervasives.(>=)
+end
+
+module Int_ops = struct
+    let op1_U2212 = Pervasives.(~-)
+    let op2_U002b = Pervasives.(+)
+    let op2_U2212 = Pervasives.(-)
+    let op2_U002a = Pervasives.( * )
+    let op2_U002f = Pervasives.(/)
+    let op2_mod = Pervasives.(mod)
+
+    let bitand = Pervasives.(land)
+    let bitor = Pervasives.(lor)
+    let bitxor = Pervasives.(lxor)
+    let bitlsl = Pervasives.(lsl)
+    let bitlsr = Pervasives.(lsr)
+    let bitasr = Pervasives.(asr)
+end
+
+module Int_misc = struct
+    let show = Pervasives.string_of_int
+end
