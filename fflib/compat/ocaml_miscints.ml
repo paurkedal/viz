0c16f5c38f53627e5d2af7921a72e794af523a03
Petter Urkedal <paurkedal@gmail.com>
2011-04-11 12:29:01 +0200

    Add nativeint, int32, and int64.

---
+++ fflib/compat/ocaml_miscints.ml
@@ -0,0 +1,52 @@
+(* Copyright 2011  Petter Urkedal
+ *
+ * This file is part of the Fform Standard Library.
+ *
+ * The Fform Standard Library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Fform is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fform.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+module type A_basic_int = sig
+    type t
+    val neg : t -> t
+    val add : t -> t -> t
+    val sub : t -> t -> t
+    val mul : t -> t -> t
+    val div : t -> t -> t
+    val rem : t -> t -> t
+    val of_int : int -> t
+    val as_int : t -> int
+end
+
+module Adapt
+    (M : sig
+	type t
+	val neg : t -> t
+	val add : t -> t -> t
+	val sub : t -> t -> t
+	val mul : t -> t -> t
+	val div : t -> t -> t
+	val rem : t -> t -> t
+	val of_int : int -> t
+	val to_int : t -> int
+    end) =
+struct
+    include M
+    let as_int = to_int
+end
+
+module Pervasive = struct
+    module Nativeint = Adapt (Nativeint)
+    module Int32 = Adapt (Int32)
+    module Int64 = Adapt (Int64)
+end
