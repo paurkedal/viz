c854892c4757170a64f63ff590672f2bb45f44eb
Petter Urkedal <paurkedal@gmail.com>
2011-04-14 21:42:06 +0200

    Update license texts to reflect the new name and URL.

--- camlviz/opkind.mli
+++ camlviz/opkind.mli
@@ -1,19 +1,19 @@
 (* Copyright 2010--2011  Petter Urkedal
  *
- * This file is part of Fform/OC <http://www.eideticdew.org/p/fform/>.
+ * This file is part of the Viz Compiler <http://www.vizlang.org/>.
  *
- * Fform/OC is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The Viz Compiler is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
  *
- * Fform/OC is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The Viz Compiler is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Fform/OC.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with the Viz Compiler.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
 (** Classification and Information about Operators *)
